841c4f8b0b5a65abb6c3b6621a7d98ede041903e
Leidan <djz209@gmail.com>
2014-03-17 13:11:20 -0700

    Updated Unity
    Using Unity3D 4.3.4f1

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -2,20 +2,20 @@
 using System.Collections;
 
 public class CameraMovement : MonoBehaviour {
-
+	
 	void Update () {
 		Vector3 screenResolution = new Vector3(Screen.width, Screen.height, 0.0f); 
-	    Vector3 mousePosition = Input.mousePosition; 
-	 
-	    mousePosition.y = Screen.height - mousePosition.y; 
-	    mousePosition.x = Screen.width - mousePosition.x; 
-	    screenResolution *= 0.5f; 
-	 
-	    Vector3 difference = mousePosition - screenResolution; 
-	    transform.Translate(difference * Time.smoothDeltaTime * -0.05f);
-	 
-	    Vector3 newPosition = transform.position; 
-	    newPosition.z = 50f; 
-	    transform.position = newPosition;
+		Vector3 mousePosition = Input.mousePosition; 
+		
+		mousePosition.z = mousePosition.z; 
+		mousePosition.x = Screen.width - mousePosition.x; 
+		screenResolution *= 0.5f;
+		
+		Vector3 difference = mousePosition - screenResolution; 
+		transform.Translate(difference * Time.smoothDeltaTime * -0.05f);
+		
+		Vector3 newPosition = transform.position; 
+		//newPosition.z = 50f; 
+		transform.position = newPosition;
 	}
 }
