9b4d39af71cccbed581b0ca6feceb95e12a5adbd
Leidan <djz209@gmail.com>
2014-03-19 12:38:19 -0700

    Added Fill Scene Script
    Fill scene will populate the scene with objects

---
+++ Assets/Scripts/FillScene.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FillScene : MonoBehaviour {
+
+	public GameObject fence;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
